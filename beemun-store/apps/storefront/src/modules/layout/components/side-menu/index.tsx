27eac99dc880c3509b4e157038074ea1d120756d
reveritnow <reveritnow@gmail.com>
2026-07-02 19:56:02 +0530

    Brand BEEMUN side menu copy

--- beemun-store/apps/storefront/src/modules/layout/components/side-menu/index.tsx
+++ beemun-store/apps/storefront/src/modules/layout/components/side-menu/index.tsx
@@ -11,7 +11,6 @@ import CountrySelect from "../country-select"
 import LanguageSelect from "../language-select"
 import { Locale } from "@lib/data/locales"
 
-
 const SideMenuItems = {
   Home: "/",
   Store: "/store",
@@ -65,7 +64,7 @@ const SideMenu = ({ regions, locales, currentLocale }: SideMenuProps) => {
                 <PopoverPanel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-[51] inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl">
                   <div
                     data-testid="nav-menu-popup"
-                    className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6"
+                    className="flex flex-col h-full bg-[rgba(23,40,29,0.88)] rounded-rounded justify-between p-6"
                   >
                     <div className="flex justify-end" id="xmark">
                       <button data-testid="close-menu-button" onClick={close}>
@@ -127,7 +126,7 @@ const SideMenu = ({ regions, locales, currentLocale }: SideMenuProps) => {
                         />
                       </div>
                       <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} Medusa Store. All rights
+                        Copyright {new Date().getFullYear()} BEEMUN. All rights
                         reserved.
                       </Text>
                     </div>
